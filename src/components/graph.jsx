483b4d9b2892248ef049968bbc721c4b18cd104a
Harish <harishnik08@gmail.com>
2023-05-17 00:02:30 +0530

    changes made

--- src/components/graph.jsx
+++ src/components/graph.jsx
@@ -1,55 +1,56 @@
 import { Box } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
+LineChart,
+Line,
+XAxis,
+YAxis,
+CartesianGrid,
+Tooltip,
+Legend,
 } from "recharts";
 
-export default function LineGraph({ expenses }) {
-  const getParsedData = (_data) => {
-    return _data.map((val) => ({
-      date: val.date,
-      amount: val.amount,
-    }));
-  };
+export default function LineGraph({ expenses, ...rest }) {
+const getParsedData = (_data) => {
+return _data.map((val) => ({
+amount: val.amount,
+category: val.category,
+}));
+};
 
-  return (
-    <Box>
-      <Box
-        bg={"white"}
-        width={"647px"}
-        p="10px"
-        border="2px solid"
-        color="black"
-      >
-        <LineChart
-          width={600}
-          height={300}
-          data={getParsedData(expenses)}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="amount" stroke="#8884d8" />
-        </LineChart>
-      </Box>
-    </Box>
-  );
+return (
+<Box {...rest}>
+<Box
+     bg="white"
+     p={4}
+     borderWidth={2}
+     borderColor="black"
+     borderRadius="md"
+     boxShadow="md"
+   >
+<LineChart
+width={600}
+height={300}
+data={getParsedData(expenses)}
+margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+>
+<CartesianGrid strokeDasharray="3 3" />
+<XAxis dataKey="category" />
+<YAxis dataKey="amount" />
+<Tooltip />
+<Legend />
+<Line type="monotone" dataKey="amount" stroke="#8884d8" />
+</LineChart>
+</Box>
+</Box>
+);
 }
 
 LineGraph.propTypes = {
-  expenses: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.string,
-      amount: PropTypes.number,
-    })
-  ),
+expenses: PropTypes.arrayOf(
+PropTypes.shape({
+date: PropTypes.string,
+amount: PropTypes.number,
+})
+),
 };
